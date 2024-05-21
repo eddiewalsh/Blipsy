c4db80598d5743aed5f5017987e22933ef33820a
eddie <eddie.walsh.dev@gmail.com>
2024-05-21 19:45:56 +0100

    Blipsy First Commit
    Established connection with the Spotify SDK

---
+++ app/src/main/java/com/example/blipsy/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.blipsy;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.PlayerState;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "client_id";
+    private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+                    }
+                });
+    }
+
+    private void connected() {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+
+    private void getCurrentTrack() {
+        // Check if SpotifyAppRemote is connected
+        if (mSpotifyAppRemote != null && mSpotifyAppRemote.isConnected()) {
+            mSpotifyAppRemote.getPlayerApi()
+                    .getPlayerState()
+                    .setResultCallback(new CallResult.ResultCallback<PlayerState>() {
+                        @Override
+                        public void onResult(PlayerState playerState) {
+                            // Handle the current player state
+                            if (playerState != null && playerState.track != null) {
+                                String currentTrackName = playerState.track.name;
+                                String currentTrackArtist = playerState.track.artist.name;
+                                Log.d("MainActivity", "Current Track: " + currentTrackName +
+                                        " by " + currentTrackArtist);
+                            } else {
+                                Log.d("MainActivity", "No track is currently playing.");
+                            }
+                        }
+                    });
+        } else {
+            Log.e("MainActivity", "SpotifyAppRemote is not connected.");
+        }
+    }
+}
